c25aeb784d1c478b617814277e07d0a3781209e7
Joseph Otine <joeotine@gmail.com>
2023-12-07 02:13:58 -0800

    All tasks completed

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,27 +1,16 @@
 #!/usr/bin/env python3
+"""Use mypy to validate the following piece of code
+and apply any necessary changes.
 """
-This is a module that provides a function for zooming in on a tuple.
-"""
-from typing import List, Tuple
+
+from typing import Tuple, List, Any
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
-    """
-    This function takes a tuple and a zoom factor, and returns a new list
-        with the elements of the tuple repeated according to the zoom factor.
-
-    Parameters:
-    lst (Tuple): The tuple to zoom in on.
-    factor (int): The zoom factor, which determines how many times each
-        element of the tuple should be repeated in the output list.
-
-    Returns:
-    List: A new list with the elements of the tuple repeated according to
-        the zoom factor.
-    """
+    """Corrected annotations"""
     zoomed_in: List = [
         item for item in lst
-        for i in range(int(factor))
+        for i in range(factor)
     ]
     return zoomed_in
 
@@ -31,6 +20,3 @@ array = (12, 72, 91)
 zoom_2x = zoom_array(array)
 
 zoom_3x = zoom_array(array, 3)
-
-# :man: Author.
-This project was done by [Joseph Otine](https://github.com/joeotine).
